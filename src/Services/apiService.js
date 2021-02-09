bb036a89d06d0a6eed0d581c23e45847e2d6267a
BotanCosar <55230904+BotanCosar@users.noreply.github.com>
2021-02-09 11:27:29 +0100

    registerAccount service added

--- src/Services/apiService.js
+++ src/Services/apiService.js
@@ -4,7 +4,17 @@ const BASE_URL = "https://iv1201-rest-server.herokuapp.com/";
 class ApiService {
 
   getPersonById(id){
-    return fetch(BASE_URL + "person/" + id).then(e => e.json());
+    return fetch(BASE_URL + "person/" + id).then(response => response.json());
+  }
+
+  registerAccount(body){
+    return fetch(BASE_URL+"person",{
+      "method":"POST",
+      "headers":{
+        'Content-Type': 'application/json'
+      },
+      "body":JSON.stringify(body)
+    }).then(response=>response.json());
   }
 
 }
