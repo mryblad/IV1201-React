0b658c238a6ccf485cf3e7c31f9f7de075ce590b
Kodeama <Kodeama@users.noreply.github.com>
2021-03-05 14:42:13 +0100

    Footer tester

--- src/__tests__/Footer.test.js
+++ src/__tests__/Footer.test.js
@@ -1,10 +1,21 @@
-/*import React from 'react'
+import React from 'react'
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import {Footer} from "./../Components/Footer/Footer";
-*/
 
 // Test to see if Jest is working
-it('Test if Jest works', () => {
-  expect(1).toBe(1)
+Enzyme.configure({ adapter: new Adapter() });
+describe('Footer', () => {
+  it('Footer images source should exist', () => {
+    const wrapper = mount(<Footer/>);
+    const images = wrapper.find("img");
+    expect(images.first().prop('src')==="se_icon.png"?true:false).toBe(true);
+    expect(images.last().prop('src')==="en_icon.png"?true:false).toBe(true);
+  });
+  it('Footer alt-text should exist', () => {
+    const wrapper = mount(<Footer/>);
+    const images = wrapper.find("img");
+    expect(images.first().prop('alt')==="se flag"?true:false).toBe(true);
+    expect(images.last().prop('alt')==="en flag"?true:false).toBe(true);
+  });
 })
